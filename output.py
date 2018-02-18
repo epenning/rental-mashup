9260186550649357fdcb2d67e5c55288d9bd0e44
epenning <epennington10@gmail.com>
2018-02-18 13:00:57 -0600

    forgot output.py file previously

---
+++ output.py
@@ -0,0 +1,17 @@
+import unicodecsv as csv
+
+filename_base = "properties-Austin-TX"
+filename_extension = ".csv"
+fieldnames = ['address', 'postal_code', 'url', 'fiber_ready', 'transit_time']
+
+
+def start_file(type, extension=filename_extension):
+    with open(filename_base + "-" + type + extension, 'wb') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+
+
+def append_file(type, row, extension=filename_extension):
+    with open(filename_base + "-" + type + extension, 'ab') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames)
+        writer.writerow(row)
